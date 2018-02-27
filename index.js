7ae40434cdd82ad1c253713c9d130a96236ad758
Onkar Chiroma <info@onkarchiroma.co.uk>
2018-02-27 17:46:11 +0000

    Add netlifycms

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.get('/admin', function(req, res) {
+  res.sendFile(__dirname + "/admin/index.html");
+});
+
+app.get('/admin/config.yml', function(req, res) {
+  res.sendFile(__dirname + "/admin/config.yml");
+});
+
 app.use(express.static('build'));
 
-app.use(function (req, res, next) {
-  res.status(404).redirect("/errors/404")
-})
+// app.use(function (req, res, next) {
+//   res.status(404).redirect("/")
+// })
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('App started :)')
